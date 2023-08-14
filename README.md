64219fdc11723788343d531914d12067a0a2b78f
mhdtaufikrm <mtaufikramadhan27@gmail.com>
2023-08-14 10:53:21 +0700

    done develop and add read.me

--- README.md
+++ README.md
@@ -1,64 +1,70 @@
-<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+---
 
-<p align="center">
-<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/dt/laravel/framework" alt="Total Downloads"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/v/laravel/framework" alt="Latest Stable Version"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
-</p>
+# JSON Data Manipulation Project
 
-## About Laravel
+This project demonstrates how to manipulate JSON data using Laravel. It fetches JSON data from two sources, manipulates it according to specified requirements, and then presents the manipulated data in a new structure.
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
+## Getting Started
 
-- [Simple, fast routing engine](https://laravel.com/docs/routing).
-- [Powerful dependency injection container](https://laravel.com/docs/container).
-- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
-- Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).
-- Database agnostic [schema migrations](https://laravel.com/docs/migrations).
-- [Robust background job processing](https://laravel.com/docs/queues).
-- [Real-time event broadcasting](https://laravel.com/docs/broadcasting).
+Follow these steps to set up and run the project:
 
-Laravel is accessible, powerful, and provides tools required for large, robust applications.
+### Prerequisites
 
-## Learning Laravel
+- [Composer](https://getcomposer.org/download/)
+- [Git](https://git-scm.com/downloads)
+- [PHP](https://www.php.net/manual/en/install.php)
 
-Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.
+### Installation
 
-If you don't feel like reading, [Laracasts](https://laracasts.com) can help. Laracasts contains over 1500 video tutorials on a range of topics including Laravel, modern PHP, unit testing, and JavaScript. Boost your skills by digging into our comprehensive video library.
+1. Clone the project repository:
 
-## Laravel Sponsors
+```bash
+git clone https://github.com/mhdtaufikr/Project2.git
+```
 
-We would like to extend our thanks to the following sponsors for funding Laravel development. If you are interested in becoming a sponsor, please visit the Laravel [Patreon page](https://patreon.com/taylorotwell).
+2. Navigate to the project directory:
 
-### Premium Partners
+```bash
+cd Project2
+```
 
-- **[Vehikl](https://vehikl.com/)**
-- **[Tighten Co.](https://tighten.co)**
-- **[Kirschbaum Development Group](https://kirschbaumdevelopment.com)**
-- **[64 Robots](https://64robots.com)**
-- **[Cubet Techno Labs](https://cubettech.com)**
-- **[Cyber-Duck](https://cyber-duck.co.uk)**
-- **[Many](https://www.many.co.uk)**
-- **[Webdock, Fast VPS Hosting](https://www.webdock.io/en)**
-- **[DevSquad](https://devsquad.com)**
-- **[Curotec](https://www.curotec.com/services/technologies/laravel/)**
-- **[OP.GG](https://op.gg)**
-- **[WebReinvent](https://webreinvent.com/?utm_source=laravel&utm_medium=github&utm_campaign=patreon-sponsors)**
-- **[Lendio](https://lendio.com)**
+3. Install the project dependencies using Composer:
 
-## Contributing
+```bash
+composer install
+```
 
-Thank you for considering contributing to the Laravel framework! The contribution guide can be found in the [Laravel documentation](https://laravel.com/docs/contributions).
+### Usage
 
-## Code of Conduct
+1. Start the Laravel development server:
 
-In order to ensure that the Laravel community is welcoming to all, please review and abide by the [Code of Conduct](https://laravel.com/docs/contributions#code-of-conduct).
+```bash
+php artisan serve
+```
 
-## Security Vulnerabilities
+2. Open Postman or any other API testing tool.
 
-If you discover a security vulnerability within Laravel, please send an e-mail to Taylor Otwell via [taylor@laravel.com](mailto:taylor@laravel.com). All security vulnerabilities will be promptly addressed.
+3. Make a GET request to the following URL:
+
+```
+http://127.0.0.1:8000/api/newStructure
+```
+
+This endpoint will return the manipulated JSON data in the new structure.
+
+### What's Happening
+
+- The project manipulates JSON data from two different sources and restructures it according to specified requirements.
+- The manipulated data is sorted based on the `ahass_distance` field in ascending order.
+
+## Credits
+
+This project was created as part of the Nawatech PHP Developer Coding Test.
 
 ## License
 
-The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+This project is open-source and available under the [MIT License](LICENSE).
+
+---
+
+You can customize the README further by adding more details, such as API documentation, additional setup instructions, and any other relevant information about the project. Remember to replace placeholders with actual content as needed.
--- app/Http/Controllers/JsonManipulationController.php
+++ app/Http/Controllers/JsonManipulationController.php
@@ -10,66 +10,199 @@ class JsonManipulationController extends Controller
 {
     public function manipulateJson()
     {
-        $json1Response = Http::get('https://pastebin.com/raw/18HKxBeM', ['password' => 'jfcX8q3ezy']);
-        $json2Response = Http::get('https://pastebin.com/raw/2fBXAWqh', ['password' => 'tQZRQ9UJH8']);
-        $json3Response = Http::get('https://pastebin.com/raw/P7xNa6Ji', ['password' => 'GDYzTGEVjR']);
-        $json4Response = Http::get('https://pastebin.com/raw/hifJjX4i', ['password' => 'G2ghZ56h6C']);
-        dd(json_decode($json1Response->body(), true));
+        $jsonData1 = [
+            "status" => 1,
+            "message" => "Data Successfully Retrieved.",
+            "data" => [
+                [
+                    "name" => "anwar",
+                    "email" => "anwar@mail.com",
+                    "booking" => [
+                        "booking_number" => "101000103066",
+                        "book_date" => "2022-03-12",
+                        "workshop" => [
+                            "code" => "01000",
+                            "name" => "Wahana Honda Gunung Sahari"
+                        ],
+                        "motorcycle" => [
+                            "name" => "NEW CB150R STREETFIRE",
+                            "ut_code" => "H5C02R20S1 M/T"
+                        ]
+                    ]
+                ],
+                [
+                    "name" => "heru",
+                    "email" => "heru@gmail.com",
+                    "booking" => [
+                        "booking_number" => "10100062661",
+                        "book_date" => "2022-06-09",
+                        "workshop" => [
+                            "code" => "11497",
+                            "name" => "AHASS KAWI Indah Jaya Motor 3"
+                        ],
+                        "motorcycle" => [
+                            "name" => "BEAT SPORTY CBS MMC",
+                            "ut_code" => "HH1B02N41S1 A/T"
+                        ]
+                    ]
+                ],
+                [
+                    "name" => "bayu",
+                    "email" => "bayu@yahoo.com",
+                    "booking" => [
+                        "booking_number" => "100190109431",
+                        "book_date" => "2022-06-10",
+                        "workshop" => [
+                            "code" => "17236",
+                            "name" => "AHASS MEGATAMA MOTOR"
+                        ],
+                        "motorcycle" => [
+                            "name" => "BEAT POP ESP CW COMIC",
+                            "ut_code" => "Y1G02N02L1A A/T"
+                        ]
+                    ]
+                ],
+                [
+                    "name" => "santoso",
+                    "email" => "santoso@microsoft.com",
+                    "booking" => [
+                        "booking_number" => "101000109430",
+                        "book_date" => "2022-03-12",
+                        "workshop" => [
+                            "code" => "07577",
+                            "name" => "AHASS TUNGGAL JAYA"
+                        ],
+                        "motorcycle" => [
+                            "name" => "BLADE S",
+                            "ut_code" => "NF11C1CD M/T"
+                        ]
+                    ]
+                ],
+                [
+                    "name" => "ilyas",
+                    "email" => "ilyas@gmail.com",
+                    "booking" => [
+                        "booking_number" => "117236109426",
+                        "book_date" => "2022-06-08",
+                        "workshop" => [
+                            "code" => "00190",
+                            "name" => "Dunia Motor Kebayoran Lama"
+                        ],
+                        "motorcycle" => [
+                            "name" => "NEW BEAT CAST WHEEL",
+                            "ut_code" => "NC11B3C2A/T"
+                        ]
+                    ]
+                ],
+                [
+                    "name" => "kibo",
+                    "email" => "kibo@gmail.com",
+                    "booking" => [
+                        "booking_number" => "117550109401",
+                        "book_date" => "2022-05-10",
+                        "workshop" => [
+                            "code" => "11497",
+                            "name" => "AHASS KAWI Indah Jaya Motor 3"
+                        ],
+                        "motorcycle" => [
+                            "name" => "BEAT STREET",
+                            "ut_code" => "D1I02N27M1 A/T"
+                        ]
+                    ]
+                ],
+                [
+                    "name" => "ilyas",
+                    "email" => "ilyas@gmail.com",
+                    "booking" => [
+                        "booking_number" => "117550109404",
+                        "book_date" => "2022-06-08",
+                        "workshop" => [
+                            "code" => "00190",
+                            "name" => "Dunia Motor Kebayoran Lama"
+                        ],
+                        "motorcycle" => [
+                            "name" => "REVO FIT JKT",
+                            "ut_code" => "R2B02K01S1K M/T"
+                        ]
+                    ]
+                ],
+            ],
+        ];
+        
+        $jsonData2 = [
+            "status" => 1,
+            "message" => "Data Successfully Retrieved.",
+            "data" => [
+                [
+                    "code" => "01000",
+                    "name" => "Wahana Honda Gunung Sahari",
+                    "address" => "Jalan Gunung Sahari",
+                    "phone_number" => "085800000000",
+                    "distance" => 5.2
+                ],
+                [
+                    "code" => "11497",
+                    "name" => "AHASS KAWI Indah Jaya Motor 3",
+                    "address" => "Jakarta Pusat",
+                    "phone_number" => "085300000000",
+                    "distance" => 10.3
+                ],
+                [
+                    "code" => "00190",
+                    "name" => "Dunia Motor Kebayoran Lama",
+                    "address" => "Kebayoran Lama, Jakarta Selatan",
+                    "phone_number" => "085600000000",
+                    "distance" => 2.5
+                ],
+                [
+                    "code" => "07577",
+                    "name" => "AHASS TUNGGAL JAYA",
+                    "address" => "Jakarta Timur",
+                    "phone_number" => "085200000000",
+                    "distance" => 11.5
+                ],
+            ],
+        ];
 
-        if ($json1Response->successful() && $json2Response->successful() && $json3Response->successful() && $json4Response->successful()) {
-            $json1Data = json_decode($json1Response->body(), true);
-            $json2Data = json_decode($json2Response->body(), true);
-            $json3Data = json_decode($json3Response->body(), true);
-            $json4Data = json_decode($json4Response->body(), true);
+        $manipulateData = [];
+        $data1List = $jsonData1['data'];
+        $data2List = $jsonData2['data'];
 
-            // Create a map of ahass code to ahass data for quick lookup
-            $ahassMap = [];
-            foreach ($json2Data['data'] as $ahass) {
-                $ahassMap[$ahass['code']] = $ahass;
+        foreach ($data1List as $index => $data1) {
+            if (isset($data2List[$index])) {
+                $data2 = $data2List[$index];
+
+                $manipulateData[] = [
+                    "name" => $data1['name'] ?? '',
+                    "email" => $data1['email'] ?? '',
+                    "booking_number" => $data1['booking']['booking_number'] ?? '',
+                    "book_date" => $data1['booking']['book_date'] ?? '',
+                    "ahass_code" => $data1['booking']['workshop']['code'] ?? '',
+                    "ahass_name" => $data2['name'] ?? '',
+                    "ahass_address" => $data2['address'] ?? '',
+                    "ahass_contact" => $data2['phone_number'] ?? '',
+                    "ahass_distance" => $data2['distance'] ?? '',
+                    "motorcycle_ut_code" => $data1['booking']['motorcycle']['ut_code'] ?? '',
+                    "motorcycle" => $data1['booking']['motorcycle']['name'] ?? ''
+                ];
             }
-
-            // Create a new structure for the manipulated JSON
-            $manipulatedJson = [
-                'status' => 1,
-                'message' => 'Data Successfully Retrieved.',
-                'data' => [],
-            ];
-
-            foreach ($json1Data['data'] as $item) {
-                $booking = $item['booking'];
-                $ahassCode = $booking['workshop']['code'];
-
-                // Retrieve AHASS data from the map
-                $ahass = $ahassMap[$ahassCode] ?? null;
-
-                if ($ahass) {
-                    $manipulatedJson['data'][] = [
-                        'name' => $item['name'],
-                        'email' => $item['email'],
-                        'booking_number' => $booking['booking_number'],
-                        'book_date' => $booking['book_date'],
-                        'ahass_code' => $ahassCode,
-                        'ahass_name' => $ahass['name'],
-                        'ahass_address' => $ahass['address'] ?? '',
-                        'ahass_contact' => $ahass['phone_number'] ?? '',
-                        'ahass_distance' => $ahass['distance'] ?? 0,
-                        'motorcycle_ut_code' => $booking['motorcycle']['ut_code'],
-                        'motorcycle' => $booking['motorcycle']['name'],
-                    ];
-                }
-            }
-
-            // Sort the data by 'ahass_distance'
-            usort($manipulatedJson['data'], function ($a, $b) {
-                return $a['ahass_distance'] <=> $b['ahass_distance'];
-            });
-
-            return response()->json($manipulatedJson);
-        } else {
-            return response()->json([
-                'error' => 'Failed to fetch JSON data',
-            ], 500);
         }
+
+        // Step 2: Sort data based on 'ahass_distance'
+        usort($manipulateData, function ($a, $b) {
+            return $a['ahass_distance'] - $b['ahass_distance'];
+        });
+
+        // Create the final $newStructure variable
+        $newStructure = [
+            "status" => 1,
+            "message" => "Data Successfully Retrieved.",
+            "data" => $manipulateData
+        ];
+
+        return response()->json($newStructure);
+
+            
     }
 }
 
